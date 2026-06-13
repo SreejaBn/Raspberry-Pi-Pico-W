134df9b1bf94962ac95b60caaa88b0d2be46066e
Sreeja Banerjee <155034501+SreejaBn@users.noreply.github.com>
2026-06-13 22:58:21 +0530

    Add button reading functionality with LED control

---
+++ Button/reading from button.py
@@ -0,0 +1,10 @@
+from machine import Pin
+from time import sleep
+
+myButton= Pin(14, Pin.IN, Pin.PULL_UP)
+led= Pin(15, Pin.OUT)
+
+while True:
+    butState= myButton.value()
+    print(butState)
+    sleep(.1)
