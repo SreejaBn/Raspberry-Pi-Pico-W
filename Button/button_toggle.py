aa987877ecc10020d2131eea061bb4867d65de12
Sreeja Banerjee <155034501+SreejaBn@users.noreply.github.com>
2026-06-14 15:21:21 +0530

    Refactor button toggle logic and add state tracking

--- Button/button_toggle.py
+++ Button/button_toggle.py
@@ -3,15 +3,17 @@ from time import sleep
 
 myButton= Pin(14, Pin.IN, Pin.PULL_UP)
 led= Pin(15, Pin.OUT)
-butPS= 0
-butCS= 0
+butPS= 0        #button previous state
+butCS= 0        #button current state
+ledS= 0         #button state
 
 while True:
     butCS= myButton.value()
     print(butCS)
     
-    if butPS ==0 and butCS== 1:
-        led.value(not led.value())
+    if butPS ==1 and butCS== 0:
+        ledS = not ledS
+        led.value(ledS)
     
     butPS= butCS
     sleep(.2)
